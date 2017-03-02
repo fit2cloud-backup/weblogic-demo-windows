da9c4e50f1e03df2edd5bb927124c33d9fc3bd91
xuwei <wei@fit2cloud.com>
2017-03-02 14:01:45 +0800

    fix bug

--- scripts/configure.bat
+++ scripts/configure.bat
@@ -1,5 +1,5 @@
 SET WLS_HOME=C:\Oracle\Middleware\Oracle_Home\wlserver\server
-SET STAGING=c:\tmp\weblogic-demo\
+SET STAGING=%cd%\..\weblogic-demo\
 SET CLS_PATH=%WLS_HOME%\lib\weblogic.jar;.
 CMD /c %WLS_HOME%\bin\setWLSEnv.cmd
 java -classpath %CLS_PATH% weblogic.Deployer -adminurl http://127.0.0.1:7001 -user weblogic -password fit2cloud -deploy -name xwtest -source %STAGING% -targets AdminServer
